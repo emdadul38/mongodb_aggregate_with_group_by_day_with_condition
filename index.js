143f6e7430030a9e624284fbc6cc3b126ff6a0a0
Emdadul Huq <emdadul225@gmail.com>
2019-06-30 14:30:37 +0600

    Create index.js
    mongoose group by day query

---
+++ index.js
@@ -0,0 +1,30 @@
+    let query = { 
+				"sender_id": mongoose.Types.ObjectId(user_id),
+				"$expr": {
+					"$and": [
+						{"$eq":  [{ $dayOfMonth: "$createdAt" }, current_day ] },
+						{"$eq":  [{ $month: "$createdAt" }, current_month ] },
+						{"$eq":  [{ $year: "$createdAt" }, current_year ] },
+					]
+				}
+			}
+    Looksee.aggregate([
+        { '$match': query },
+        {
+            '$group' : {
+                _id: {
+                    day: { $dayOfMonth: '$createdAt' },
+                    month: { $month: '$createdAt' },
+                    year: { $year: '$createdAt' }
+                },
+                count: { $sum: 1 }
+            }
+        },
+        {'$project': {
+            count: 1,
+            _id: 0
+        }}
+                        
+    ], async function(err, data){
+        
+    }
